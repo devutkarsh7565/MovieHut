223570b496b034047aa68cc1572692d524a2b4d4
devutkarsh7565 <utkarshtiwari200389@gmail.com>
2022-07-21 23:25:09 +0530

    fixing env

--- src/Hooks/firebase.js
+++ src/Hooks/firebase.js
@@ -3,12 +3,25 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import firebaseConfig from "../firebaseConfig";
+
+const apiKey = process.env.REACT_APP_FIREBASE_API_KEY;
+const appId = process.env.REACT_APP_APP_ID;
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+console.log(apiKey);
+const firebaseConfig = {
+  apiKey,
+  authDomain: "moviehut-7d251.firebaseapp.com",
+  projectId: "moviehut-7d251",
+  storageBucket: "moviehut-7d251.appspot.com",
+  messagingSenderId: "265151157247",
+  appId,
+  measurementId: "G-WGDK50C19S",
+};
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
