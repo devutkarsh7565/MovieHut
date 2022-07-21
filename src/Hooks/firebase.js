ce4c22bbcc80c6309856901d6ce7d92000d8db92
devutkarsh7565 <utkarshtiwari200389@gmail.com>
2022-07-21 22:39:52 +0530

    fix_login_page

--- src/Hooks/firebase.js
+++ src/Hooks/firebase.js
@@ -3,20 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import firebaseConfig from "../firebaseConfig";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCAv7M7OArTYEu1Ns4PvmGJHHK9gTrO2KA",
-  authDomain: "moviehut-7d251.firebaseapp.com",
-  projectId: "moviehut-7d251",
-  storageBucket: "moviehut-7d251.appspot.com",
-  messagingSenderId: "265151157247",
-  appId: "1:265151157247:web:e2b7a375e58c66c4514dca",
-  measurementId: "G-WGDK50C19S",
-};
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
